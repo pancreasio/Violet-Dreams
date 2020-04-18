532bb26f65dd792926cefd58396d50114cae31bd
fedepvg <federicopvg@gmail.com>
2020-04-18 18:03:57 -0300

    car rotation implemented

---
+++ Violet Dreams/Assets/Scripts/CarController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarController : CarMovement
+{
+    void Update()
+    {
+        Vector2 movementVector = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+
+        if (movementVector != Vector2.zero && movementVector!= newDirection)
+        {
+            newDirection = movementVector;
+            float angle = Vector2.SignedAngle(transform.up, movementVector);
+            newRotationAngle = transform.eulerAngles.z + angle;
+            Debug.Log(angle);
+        }
+    }
+
+    public override void FixedUpdate()
+    {
+        base.FixedUpdate();
+    }
+}
---
+++ Violet Dreams/Assets/Scripts/CarMovement.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarMovement : MonoBehaviour
+{
+    public float speed;
+    public float acceleration;
+    public float deacceleration;
+    
+    protected Vector2 newDirection;
+    protected float newRotationAngle;
+
+    bool turning;
+    Vector2 direction;
+    float rotationAngle;
+    Rigidbody2D rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    public virtual void FixedUpdate()
+    {
+        if(newDirection == direction)
+        {
+            if (rb.velocity.magnitude < speed)
+                rb.AddRelativeForce(Vector2.up * acceleration);
+        }
+        else
+        {
+            if (turning)
+            {
+                StopCoroutine("Turn");
+                direction = newDirection;
+                turning = false;
+            }
+            rotationAngle = transform.eulerAngles.z;
+
+            StartCoroutine("Turn");
+        }
+    }
+
+    IEnumerator Turn()
+    {
+        turning = true;
+        float timer = 0f;
+        float fromAngle = rotationAngle;
+        float toAngle = newRotationAngle;
+        direction = newDirection;
+        rotationAngle = newRotationAngle;
+        while (turning)
+        {
+            float zAngle = Mathf.LerpAngle(fromAngle, toAngle, timer);
+            transform.eulerAngles = new Vector3(0f, 0f, zAngle);
+
+            timer += Time.deltaTime;
+
+            if (timer >= 1f)
+            {
+                direction = newDirection;
+                rotationAngle = newRotationAngle;
+                turning = false;
+            }
+
+            yield return null;
+        }
+    }
+}
