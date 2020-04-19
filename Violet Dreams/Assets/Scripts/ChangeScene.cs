1616243974cfee9e3b850a82c58297a834532aa2
fedepvg <federicopvg@gmail.com>
2020-04-19 20:24:19 -0300

    menu and credits added

---
+++ Violet Dreams/Assets/Scripts/ChangeScene.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ChangeScene : MonoBehaviour
+{
+    public void LoadScene(string destScene)
+    {
+        SceneManager.LoadScene(destScene);
+    }
+
+    public void ExitGame()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#else
+        Application.Quit ();
+#endif
+    }
+}
