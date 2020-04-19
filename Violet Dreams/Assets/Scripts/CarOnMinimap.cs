1421ccb86cf16519a5e5360065a5781c527a5a81
fedepvg <federicopvg@gmail.com>
2020-04-19 19:38:50 -0300

    minimap added

---
+++ Violet Dreams/Assets/Scripts/CarOnMinimap.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CarOnMinimap : MonoBehaviour
+{
+    public GameObject car;
+
+    void Update()
+    {
+        transform.position = car.transform.position;
+    }
+}
