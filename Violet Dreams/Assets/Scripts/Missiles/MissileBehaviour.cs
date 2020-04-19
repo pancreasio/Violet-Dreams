d196436f08d83e94563031fb759e7ff6bede8ed3
PatoDL <patodl2701@gmail.com>
2020-04-19 14:41:39 -0300

    missile system included

---
+++ Violet Dreams/Assets/Scripts/Missiles/MissileBehaviour.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileBehaviour : MonoBehaviour
+{
+    public delegate GameObject OnObjectiveSelected();
+    public static OnObjectiveSelected SelectObjective;
+
+    public delegate void OnMissileCollided();
+    public static OnMissileCollided DeactiveMissile;
+
+    Transform target;
+
+    public Transform van;
+
+    Rigidbody rig;
+
+    public Vector3 originalRot;
+
+    public float speed;
+    public float rotationSpeed;
+
+    float pursueTimer;
+    public float timeBeforePursue;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = GetComponent<Rigidbody>();
+        pursueTimer = timeBeforePursue;
+    }
+
+    private void OnEnable()
+    {
+        lerpModifier = 0.0f;
+        transform.position = van.position;
+        transform.rotation = Quaternion.Euler(originalRot);
+        target = SelectObjective().transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(pursueTimer > 0f)
+            pursueTimer -= Time.deltaTime;
+    }
+
+    float lerpModifier = 0.0f;
+
+    void FixedUpdate()
+    {
+        if (pursueTimer <= 0f)
+        {
+            transform.rotation = Quaternion.Lerp(Quaternion.Euler(originalRot),
+                Quaternion.LookRotation((target.transform.position - transform.position).normalized,Vector3.up), lerpModifier);
+            lerpModifier += Time.fixedDeltaTime/rotationSpeed;
+        }
+        rig.velocity = transform.forward * speed * Time.fixedDeltaTime;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        DeactiveMissile();
+        pursueTimer = timeBeforePursue;
+        rig.velocity = Vector3.zero;
+        gameObject.SetActive(false);
+    }
+}
---
+++ Violet Dreams/Assets/Scripts/Missiles/MissileManager.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MissileManager : MonoBehaviour
+{
+    GameObject objective;
+
+    public Camera cam;
+
+    public GameObject missile;
+
+    bool isMissileActive = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        MissileBehaviour.SelectObjective = SetMissileTarget;
+        MissileBehaviour.DeactiveMissile = GetMissileDeactivated;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0) && !isMissileActive)
+        {
+            objective = SetObjective();
+            isMissileActive = true;
+            missile.SetActive(true);
+        }
+    }
+
+    GameObject SetObjective()
+    {
+        RaycastHit hit;
+        Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, 100.0f))
+        {
+            Debug.Log("You selected the " + hit.transform.name);
+            return hit.transform.gameObject;
+        }
+        else return null;
+    }
+
+    GameObject SetMissileTarget()
+    {
+        return objective;
+    }
+
+    void GetMissileDeactivated()
+    {
+        isMissileActive = false;
+    }
+}
