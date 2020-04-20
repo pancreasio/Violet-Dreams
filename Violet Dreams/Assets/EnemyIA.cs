04495fe8cc833c35b2d7ecbda9bebc7da7cc2890
fedepvg <federicopvg@gmail.com>
2020-04-20 13:20:31 -0300

    truck now falls

--- Violet Dreams/Assets/EnemyIA.cs
+++ Violet Dreams/Assets/EnemyIA.cs
@@ -7,18 +7,41 @@ public class EnemyIA : MonoBehaviour
 {
     public Transform target;
     private NavMeshAgent agent;
-
+    Rigidbody rb;
     bool navmeshStarted = false;
 
     // Start is called before the first frame update
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        agent.SetDestination(target.position);
+        if (agent.enabled)
+        {
+            agent.SetDestination(target.position);
+
+            bool blocked = false;
+            NavMeshHit nmHit;
+            if (agent.FindClosestEdge(out nmHit))
+            {
+                if (nmHit.distance < agent.radius)
+                    blocked = true;
+            }
+
+            if (blocked)
+            {
+                rb.velocity = agent.velocity;
+                agent.enabled = false;
+            }
+        }
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        agent.enabled = true;
     }
 }
