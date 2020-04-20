eebaba8f1e873a0329f2a467b60947dbe20a356d
pancreasio <julianholisu@gmail.com>
2020-04-20 18:01:32 -0300

    Most Zones Completed

--- Violet Dreams/Assets/Scripts/Game Flow/SeedSpot.cs
+++ Violet Dreams/Assets/Scripts/Game Flow/SeedSpot.cs
@@ -5,14 +5,20 @@ using UnityEngine;
 public class SeedSpot : MonoBehaviour
 {
     bool seeded;
+    public GameObject deadZone;
+    public GameObject liveZone;
 
     private void Start()
     {
+        deadZone.SetActive(true);
+        liveZone.SetActive(false);
         seeded = false;
     }
 
     public void Seed()
     {
+        deadZone.SetActive(false);
+        liveZone.SetActive(true);
         seeded = true;
     }
 }
