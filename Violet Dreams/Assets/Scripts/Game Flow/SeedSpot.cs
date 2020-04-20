d4a98630ede900c7a167a189fec908defa7647d0
pancreasio <julianholisu@gmail.com>
2020-04-20 17:12:21 -0300

    Live and Dead zones added

--- Violet Dreams/Assets/Scripts/Game Flow/SeedSpot.cs
+++ Violet Dreams/Assets/Scripts/Game Flow/SeedSpot.cs
@@ -5,14 +5,21 @@ using UnityEngine;
 public class SeedSpot : MonoBehaviour
 {
     bool seeded;
+    public GameObject deadZone;
+    public GameObject liveZone;
+
 
     private void Start()
     {
         seeded = false;
+        deadZone.SetActive(true);
+        liveZone.SetActive(false);
     }
 
     public void Seed()
     {
         seeded = true;
+        deadZone.SetActive(false);
+        liveZone.SetActive(true);
     }
 }
